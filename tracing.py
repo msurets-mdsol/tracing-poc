2590129e66fa20cf8b073170e3193eef5d168e06
Maxim Surets <msurets@mdsol.com>
2025-04-14 15:54:30 -0400

    Minimal setup for tracing

--- tracing.py
+++ tracing.py
@@ -1,5 +1,5 @@
 import motel
-from opentelemetry import trace, baggage
+from opentelemetry import trace
 from time import sleep
 
 try:
@@ -8,12 +8,10 @@ except Exception as inst:
     print("Failed to setup otel", inst)
 
 tracer = trace.get_tracer(__name__)
-# x = tracer.start_as_current_span("test-operation")
 
-with tracer.start_span(name="test_root_span") as root_span:
-    sleep(2)
-    ctx = baggage.set_baggage("test", "test_value")
-
-
-print(f"Global context baggage: {baggage.get_all()}")
-print(f"Span context baggage: {baggage.get_all(context=ctx)}")
+with tracer.start_as_current_span(name="test_root_span") as root_span:
+    sleep(0.5)
+    with tracer.start_as_current_span("test-operation"):
+        # Call other services
+        sleep(2)
+    sleep(0.5)
